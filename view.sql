6212c040abd224b17f140dd427b8c77004230d11
DariaGera <43523466+DariaGera@users.noreply.github.com>
2020-05-02 23:18:11 +0300

    Update view.sql

--- view.sql
+++ view.sql
@@ -7,3 +7,19 @@ CREATE OR REPLACE VIEW full_accident_table AS
     FROM
              accidento
         JOIN placce ON accidento.place_id = placce.place_id;
+ 
+ 
+ 
+ 
+ 
+--creating--new--right--view---------------------------------------------
+CREATE OR REPLACE VIEW full_table AS
+    SELECT
+        place.town_town,
+        place.address,
+        accident.timestamp,
+        accident.title
+    FROM
+             accident
+        JOIN place ON accident.place_place_id = place.place_id;    
+        
